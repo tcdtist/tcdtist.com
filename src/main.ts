fac8fbd7487b58130d363f21efefd983b78ee047
tcdtist <chithanh.tist@gmail.com>
2024-09-12 19:54:21 +0700

    revert(package): remove "sentry"

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,7 @@
-import * as Sentry from '@sentry/vue'
-import { inject } from '@vercel/analytics'
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
+import { inject } from '@vercel/analytics'
 import './style.css'
+import App from './App.vue'
 
-const app = createApp(App)
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [], // Thêm các route của bạn vào đây
-})
-
-Sentry.init({
-  app,
-  dsn: 'https://5fac7883db9d5b518653c8ccb9c356d7@o4506076619014144.ingest.us.sentry.io/4507256096948224',
-  integrations: [Sentry.browserTracingIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-})
-
-app.use(router)
+createApp(App).mount('#app')
 inject()
-app.mount('#app')
