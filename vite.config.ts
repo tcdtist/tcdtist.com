c9c3c6a0b6b6d2d95cec58085315634b3fc987d7
tcdtist <chithanh.tist@gmail.com>
2024-09-12 20:18:42 +0700

    preload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router'],
+  },
 })
