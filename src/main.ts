aad7f5675b7ab71289a072141b175d763b3484f5
tcdtist <chithanh.tist@gmail.com>
2024-09-13 20:59:33 +0700

    check yarn

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
-import { inject } from '@vercel/analytics'
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 app.mount('#app')
-inject()
